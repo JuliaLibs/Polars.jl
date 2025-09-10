73b65e89dd297e02024f71b2333616a368eede04
Clouds Flowing <clouds.flowing@gmail.com>
2025-09-10 23:08:43 +0800

    add polars_version

--- polars-jll/src/lib.rs
+++ polars-jll/src/lib.rs
@@ -1,5 +1,20 @@
-use jlrs::prelude::*;
+use core::panic;
+
+use jlrs::{data::managed::string::StringRet, prelude::*, weak_handle};
 
 julia_module!{
-    become julia_module_polars_init_fn
+  become julia_module_polars_init_fn;
+
+  fn polars_version() -> StringRet;
+}
+
+pub fn polars_version() -> StringRet {
+  let version = polars::VERSION.to_string();
+  match weak_handle!() {
+    Ok(handle) => {
+      let s = JuliaString::new(handle, version);
+      s.leak()
+    },
+    Err(_) => panic!("Could not create weak handle to Julia."),
+  }
 }
--- src/Polars.jl
+++ src/Polars.jl
@@ -4,9 +4,19 @@ using JlrsCore.Wrap
 # using libpolars_jll
 # export libpolars_jll
 
-const libpolars_local = joinpath(@__DIR__, "../target/debug/libpolars.dylib")
+@static if Sys.isapple()
+  const libpolars_soname = "libpolars.dylib"
+elseif Sys.iswindows()
+  const libpolars_soname = "libpolars.dll"
+else
+  const libpolars_soname = "libpolars.so"
+end
+const libpolars_local = joinpath(@__DIR__, "../target/debug", libpolars_soname)
 @static if isfile(libpolars_local)
   const libpolars = libpolars_local
+else
+  # fallback to soname so the system loader can find the library on the library search path
+  const libpolars = libpolars_soname
 end
 @wrapmodule(libpolars, :julia_module_polars_init_fn)
 
