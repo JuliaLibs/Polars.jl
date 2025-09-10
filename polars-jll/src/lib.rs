b34ccc84e1124dfab22d79cfbf42bcfe5bcac82e
Clouds Flowing <clouds.flowing@gmail.com>
2025-09-10 19:00:30 +0800

    init polars-jll

---
+++ polars-jll/src/lib.rs
@@ -0,0 +1,5 @@
+use jlrs::prelude::*;
+
+julia_module!{
+    become julia_module_polars_init_fn
+}
--- src/Polars.jl
+++ src/Polars.jl
@@ -1,5 +1,17 @@
 module Polars
 
-greet() = print("Hello World!")
+using JlrsCore.Wrap
+# using libpolars_jll
+# export libpolars_jll
+
+const libpolars_local = joinpath(@__DIR__, "../target/debug/libpolars.dylib")
+@static if isfile(libpolars_local)
+  const libpolars = libpolars_local
+end
+@wrapmodule(libpolars, :julia_module_polars_init_fn)
+
+function __init__()
+  @initjlrs
+end
 
 end # module Polars
